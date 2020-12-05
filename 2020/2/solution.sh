f3f25e5954a2885ca4f344c0895d81d95804a679
Adrian Dvergsdal <github.com@konto.atmoz.no>
2020-12-05 09:58:05 +0100

    Simpler shell scripts

--- 2020/2/solution.sh
+++ 2020/2/solution.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 awk -f part1.awk input
 awk -f part2.awk input
--- 2020/3/solution.sh
+++ 2020/3/solution.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 awk -f solution.awk -v dx=1 -v dy=1 input
 awk -f solution.awk -v dx=3 -v dy=1 input
 awk -f solution.awk -v dx=5 -v dy=1 input
